70da1e7fc75a24257f5f41a22311236779b80386
Fernando Torres <webmasterft@gmail.com>
2024-09-12 21:08:04 -0500

    Rename Icons

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -6,10 +6,13 @@ const TodoItem = ({ todo }) => {
   const { id, title, completed } = todo;
   return (
     <article id={id}>
-      <button className="flex-none">
-        <span className="rounded-full w-[24px] h-[24px] border-2 block shrink-0 border-gray3">
-          <IconCheck />
-        </span>
+      {/* <button className="inline-block rounded-full w-5 h-5 border-2 block shrink-0 border-gray3 flex-none bg-gradient-to-r from-cyan-500 to-fuchsia-500 flex items-center justify-center">
+        <IconCheck />
+      </button> */}
+      <button
+        className={`rounded-full w-5 h-5 border-2 block shrink-0 border-gray3 flex-none flex items-center justify-center ${completed && 'bg-gradient-to-r from-cyan-500 to-fuchsia-500'} `}
+      >
+        {completed && <IconCheck />}
       </button>
       <p className={`grow text-purple ${completed && 'line-through'}`}>
         {title}
--- src/components/icons/IconCheck.jsx
+++ src/components/icons/IconCheck.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-const IconCheck = () => {
+const IconCheck = ({ ...props }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9">
-      <path
-        fill="none"
-        stroke="#FFF"
-        strokeWidth="2"
-        d="M1 4.304L3.696 7l6-6"
-      />
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="11"
+      height="9"
+      fill="none"
+      stroke="#FFF"
+      strokeWidth="2"
+    >
+      <path d="M1 4.304L3.696 7l6-6" />
     </svg>
   );
 };
